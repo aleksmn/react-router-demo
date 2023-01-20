39ae51fb1375fab1c8fd60a510b875edcf5e7d61
aleksmn <al-mn@yandex.ru>
2023-01-20 18:54:58 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+);
+
 registerServiceWorker();
